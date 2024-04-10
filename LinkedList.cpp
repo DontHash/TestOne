d9ce26deff9c84d8410cef92a50d914b09a9d139
Bhishma <prakash.rhythm780@gmail.com>
2024-04-10 10:32:45 +0545

    ALmost  completed Functions

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,5 +1,16 @@
 #inlcude"LinkedList.h"
 
+LinkedeList() : HEAD(nullptr),TAIL(nullptr) {}
+
+bool LinkedList::isEmpty()
+{
+    if(HEAD==nullptr && TAIL == nullptr)
+    {
+        return true;
+    }
+    else return false;
+}
+
 void LinkedList::add(int data)
 {
 
@@ -7,9 +18,33 @@ void LinkedList::add(int data)
 
 void LinkedList::addToHead(int data)
 {
-    Node newNode;
-    newNode->data= data;
-    newNode->next= HEAD;
-    HEAD = newNode;
+    if(!isEmpty())
+    {
+         Node newNode;
+        newNode->data= data;
+        newNode->next= &HEAD;
+        HEAD = newNode;
+    }
+    else
+        newNode->data = data;
+        newNode->next = nullptr;
+        HEAD = newNode;
+
+}
+
+void LinkedList::addToTail(int data)
+{
+    if(!isEmpty())
+    {
+         Node newNode;
+        newNode->data= data;
+        newNode->next= nullptr;
+        TAIL->next = &newNode;
+        TAIL = newNode;
+    }
+    else
+        newNode->data = data;
+        newNode->next = nullptr;
+        TAIL = newNode;
 
 }
