f45f80ee0c294b0ff68ba0b7db99f976665b10ca
Bhishma <prakash.rhythm780@gmail.com>
2024-04-10 10:02:05 +0545

    An abstract form was Completed

--- LinkedList.h
+++ LinkedList.h
@@ -1,4 +1,3 @@
-#define NULL 0
 
 class Node
 {
@@ -7,6 +6,27 @@ class Node
         Node *next;
 
         Node(){}
-        Node(int d): data(d),next(NULL) {}
+        Node(int d): data(d),next(nullptr) {}
         Node(int d,Node*next): data(d),next(next) {}
+};
+
+class LinkedList
+{
+    private:
+        Node *HEAD;
+        Node *TAIL;
+    
+    public:
+        LinkedList();
+        ~LinkedList();
+
+        void add(Node *pred,int data);
+        void addToHead(int data);
+        void addToTail(int data);
+
+        bool remove(int data);
+        bool removeFromHead();
+        bool removeFromTail();
+        bool isEmpty();
+
 };
