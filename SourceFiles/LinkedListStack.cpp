ed75cfbed7990cf2b4c6b4e7913b174a4c9ef3ec
Bhishm <prakash.rhythm780@gmai.com>
2024-05-20 12:26:05 +0545

    Debugged and Completed Linked List Stack

--- SourceFiles/LinkedListStack.cpp
+++ SourceFiles/LinkedListStack.cpp
@@ -3,57 +3,70 @@
 #include "LinkedList.h"
 #include <iostream>
 
-LinkedListStack::LinkedListStack(){}
-LinkedListStack::~LinkedListStack(){}
+LinkedListStack::LinkedListStack() {}
+LinkedListStack::~LinkedListStack() {}
 
 bool LinkedListStack::isEmpty()
 {
-    if(list.isEmpty())
+    if (list.isEmpty())
     {
-        
+
         return true;
     }
     else
-    return false;
+        return false;
 }
 
 bool LinkedListStack::push(int data)
 {
-        list.addToHead(data);
-    
+    list.addToHead(data);
 }
 
-bool LinkedListStack::pop(int &data)
+void LinkedListStack::pop(int &data)
 {
-    if(!isEmpty())
+    if (!isEmpty())
     {
         list.removeFromHead(data);
     }
     else
-    std::cout<<"The Stack is empty \n";
+        std::cout << "The Stack is empty \n";
 }
 
 void LinkedListStack::top(int &data)
 {
-    if(!isEmpty())
+    if (!isEmpty())
     {
         list.HeadReturn(data);
     }
 }
 
+void LinkedListStack::traverse()
+{
+    if (!isEmpty())
+    {
+        
+        list.Traverse();
+    
+    }
+    else
+        std::cout << "The Stack is empty";
+}
 
 int main()
 {
     LinkedListStack Stack;
     int data;
-    if(Stack.isEmpty())
+    if (Stack.isEmpty())
     {
-        std::cout<<"The stack is Empty \n";
+        std::cout << "The stack is Empty \n";
     }
     Stack.push(5);
     Stack.push(6);
+    Stack.push(7);
+    Stack.push(8);
     Stack.pop(data);
-    std::cout<<"The removed data is "<<data<<std::endl;
+    std::cout << "The removed data is " << data << std::endl;
     Stack.top(data);
-    std::cout<<"The data  at the top is "<<data;
+    std::cout << "The data  at the top is \n" << data;
+    Stack.traverse();
 }
